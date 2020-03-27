a421db7157f0dc807ec2ceae1177ca3de69b8d8c
Stiven Trujillo <strujillo1293@gmail.com>
2020-03-26 23:52:08 -0500

    add configuration server

--- index.js
+++ index.js
@@ -1,62 +1,23 @@
+require('dotenv').config();
 const { GraphQLServer} = require('graphql-yoga');
-
-const typeDefs = `
-    type Query{
-        hello(name: String!): String!
-        getUsers:[User]!
-        getUser(id: ID!): User!
-    }
-
-    type Mutation{
-        createUser(name:String!, age:Int!): User!
-        updateUser(id: ID!, name:String!, age:Int!): String!
-        deleteUser(id: ID!): String!
-    }
-
-    type User{
-        id:Int!
-        name:String!
-        age:Int!
-    }
-`;
+const { importSchema } = require('graphql-import');
+const resolvers = require('./src/resolvers');
 
 
-let users = [];
+const mongoose = require('mongoose');
 
-const resolvers = {
-    Query:{
-        hello:(root, params, context, info) => `Hola ${params.name}`,
-        getUsers: (root, params, context, info) => users,
-        getUser: (root, {id}, context, info) => users.find(u => u.id == id),
-    },
-    Mutation:{
-        createUser: (root, { name,  age }, context, info) => {
-            const user = {
-                id: users.length + 123214,
-                name,
-                age,
-            };
-            users.push(user);
-            return user;
-        },
-        updateUser: (root, { id, name, age }, context, info) => {
-            let message = 'No existe el usuario';
-            users.forEach( (user) => {
-                if (user.id == id) {
-                    user.name = name;
-                    user.age = age;
-                    message = 'Updated'
-                }
-            })
-            return message;
-        },
-        deleteUser: (root, { id }, context, info) => {
-            let message = 'deleted'
-            users = users.filter(u => u.id != id);
-            return message;
-        }
-    },
-};
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const mongo = mongoose.connection;
+
+mongo.on('error', error => console.log(error))
+    .once('open', () => console.log('Connected to Database! '));
+
+const typeDefs = importSchema( __dirname + '/schema.graphql');
 
 const server = new GraphQLServer({
     typeDefs,
