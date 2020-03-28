2fded702f4c262d8c5001a0bc02eb12583a80e8b
Stiven Trujillo <strujillo1293@gmail.com>
2020-03-28 16:00:27 -0500

    delete author, get author, and eslint errors fixed

--- src/resolvers/AuthorResolvers/Query.js
+++ src/resolvers/AuthorResolvers/Query.js
@@ -1,9 +1,14 @@
-const { getAllAuthors, getOneAuthorById } = require('../../services/AuthorService');
+const { getAllAuthors, getOneAuthorById, getAllDeletedAuthors } = require('../../services/AuthorService');
 
 const getAuthors = async () => {
     const authors = await getAllAuthors();
     return authors;
 };
+
+const getDeletedAuthors = async () => {
+    const authors = await getAllDeletedAuthors();
+    return authors;
+}
 
 const getAuthorById = async (_, { id }) => {
     const author = await getOneAuthorById( id );
@@ -12,5 +17,6 @@ const getAuthorById = async (_, { id }) => {
 
 module.exports = {
     getAuthors,
+    getDeletedAuthors,
     getAuthorById,
 };
--- src/services/AuthorService.js
+++ src/services/AuthorService.js
@@ -2,23 +2,27 @@ const Authors = require('../models/Authors');
 
 const getAllAuthors = () => Authors.find({is_active: true});
 
-const getDeletedAuthors = () => Authors.find({is_active: false});
+const getAllDeletedAuthors = () => Authors.find({is_active: false});
 
 const getOneAuthorById = ( id ) => Authors.findById( id );
 
 const createOneAuthor = ( data ) => Authors.create( data );
 
-const updateById = ( id, data ) => Authors.findOneAndUpdate( {
-    _id: id, is_active: true 
-}, { ...data }, { new: true} );
+const updateById = ( id, data ) => Authors.findOneAndUpdate(
+    { _id: id, is_active: true },
+    { ...data },
+    { new: true}
+);
 
-const deleteById = (id) => Authors.findByIdAndUpdate( {
-    _id: id, is_active: true 
-}, { is_active: false }, { new: true } );
+const deleteById = (id) => Authors.findByIdAndUpdate( 
+    { _id: id, is_active: true }, 
+    { is_active: false }, 
+    { new: true }
+);
 
 module.exports = {
     getAllAuthors,
-    getDeletedAuthors,
+    getAllDeletedAuthors,
     getOneAuthorById,
     createOneAuthor,
     updateById,
