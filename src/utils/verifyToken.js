7e7b5681b48beed1ede15deeb2099cb3637d1556
Stiven Trujillo <strujillo1293@gmail.com>
2020-04-05 23:23:42 -0500

    Utils Verify Token

--- src/utils/verifyToken.js
+++ src/utils/verifyToken.js
@@ -1,20 +1,20 @@
 const jwt = require('jsonwebtoken');
 const { getOneAuthorByEmail } = require('../services/AuthorService');
 
-const verifyToken = async req => {
+const verifyToken = async req => { // request.
     try {
-        const Authorization = req.get('Authorization');
+        const Authorization = req.get('Authorization'); // se puede obtener el valor dentro de un objeto utilizando get.
         if (Authorization) {
-            // JWT sadsdfadsds.sadsdasdad.sasdadfdsfsd
-            const formatedToken = Authorization.replace('JWT ','');
-            // sadsdfadsds.sadsdasdad.sasdadfdsfsd
-            const payload = jwt.verify(formatedToken, process.env.SECRET_KEY_JWT);
-            if(!payload) return req;
-            const userAuth = await getOneAuthorByEmail(payload.email);
+            // formato: JWT sadsdfadsds.sadsdasdad.sasdadfdsfsd
+            const formatedToken = Authorization.replace('JWT ',''); // reemplazamos JWT por una cadena vacia por que eso no lo vamos a necesitar.
+            // nos quedaría así: sadsdfadsds.sadsdasdad.sasdadfdsfsd
+            const payload = jwt.verify(formatedToken, process.env.SECRET_KEY_JWT); // verify: verifica en el token (si el tiempo acabó).
+            if(!payload) return req; // si nuestro payload no existe vamos a retornar nuestro request así solito.
+            const userAuth = await getOneAuthorByEmail(payload.email); // si si, traigame el usuario.
             if (!userAuth) return req;
-            return userAuth;
+            return userAuth; // devuelvame el usuario completo. (quedará guardado en nuestro contexto)
         } else {
-            return {};
+            return {}; // regresariamos al userAuth del contexto un objeto vacio.
         }
     }catch (e) {
         throw new Error(e.message);
