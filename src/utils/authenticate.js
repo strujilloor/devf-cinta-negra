2d160b37b240c607bc694ec1545ff04c78f49500
Stiven Trujillo <strujillo1293@gmail.com>
2020-04-05 23:22:53 -0500

    Utils: Authenticate

--- src/utils/authenticate.js
+++ src/utils/authenticate.js
@@ -1,15 +1,15 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt'); // para desencriptar
 const { getOneAuthorByEmail } = require('../services/AuthorService');
-const createToken = require('./createToken');
+const createToken = require('./createToken'); // generar el token para devolverselo a quien este haciendo la petición
 
 const authenticate = ({email, password}) => {
     return new Promise((resolve, reject)=>{
         getOneAuthorByEmail(email).then(userAuth => {
             if(!userAuth) reject(new Error('Author not exist'));
-            bcrypt.compare(password, userAuth.password, (err, isValid) => {
+            bcrypt.compare(password, userAuth.password, (err, isValid) => { // compara el password con el valor almacenado encriptado
                 if(err) reject(new Error('Error to compare'));
-                isValid
-                    ? resolve(createToken(userAuth))
+                isValid // es valida la contraseña ?
+                    ? resolve(createToken(userAuth)) // creame el token con el usuario a authenticar
                     : reject(new Error('Incorrect Password'));
             });
         }); 
